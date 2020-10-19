a486d0a6c1d5bd4a2f4f392e92ab4a1d32b70b25
Yehor Pliasov <fenix4088@gmail.com>
2020-10-19 20:57:55 +0200

    Markup for Categories page created

--- dist/app.bundle.js
+++ dist/app.bundle.js
@@ -92,7 +92,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7804f670894a392b1ad5";
+/******/ 	var hotCurrentHash = "51dccf8cd61c0c4f7c00";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/pages/categories/index.js
+++ src/pages/categories/index.js
@@ -1,27 +1,27 @@
-import fetchJson from "../../utils/fetch-json.js";
+import fetchJson from '../../utils/fetch-json.js';
 
 export default class Page {
-    element;
-    components = {};
-    subElements = {};
+  element;
+  components = {};
+  subElements = {};
 
-    async render() {
-        // ? Вопрос насчет записи
-        const [response] = await Promise.all([this.getCategoriesData()]);
+  async render() {
+    // ? Вопрос насчет записи
+    const [response] = await Promise.all([this.getCategoriesData()]);
 
-        const element = document.createElement('div');
-        element.innerHTML = this.template;
-        this.element = element.firstElementChild;
+    const element = document.createElement('div');
+    element.innerHTML = this.template;
+    this.element = element.firstElementChild;
 
-        this.subElements = this.getSubElements(this.element);
-        this.createCategorieWrap(response);
+    this.subElements = this.getSubElements(this.element);
+    this.createCategorieWrap(response);
+    this.initEventListeners();
 
-        return this.element;
-        
-    }
+    return this.element;
+  }
 
-    get template() {
-        return `  
+  get template() {
+    return `  
         <div class="categories">
         <div class="content__top-panel">
           <h1 class="page-title">Categories</h1>
@@ -31,39 +31,69 @@ export default class Page {
         </div>
         </div>
       `;
-    }
+  }
 
-    async getCategoriesData() {
-        const url = this.getRequestLink('/api/rest/categories');
-        const request = await fetchJson(url);
-        return request;
-    }
+  async getCategoriesData() {
+    const url = this.getRequestLink('/api/rest/categories');
+    const request = await fetchJson(url);
+    return request;
+  }
 
-    getRequestLink(requestString) {
-        const newUrl = new URL(requestString, `${process.env.BACKEND_URL}`);
-        newUrl.searchParams.set('_sort', 'weight');
-        newUrl.searchParams.set('_refs', 'subcategory');
+  getRequestLink(requestString) {
+    const newUrl = new URL(requestString, `${process.env.BACKEND_URL}`);
+    newUrl.searchParams.set('_sort', 'weight');
+    newUrl.searchParams.set('_refs', 'subcategory');
 
-        return newUrl.href;
-    }
+    return newUrl.href;
+  }
 
-    createCategorieWrap(data = []) {
-        console.log(data);
-        const {categoriesContainer} = this.subElements;
+  createCategorieWrap(data = []) {
+    const { categoriesContainer } = this.subElements;
 
-        categoriesContainer.innerHTML = `<div class="category" data-id="bytovaya-texnika">
-        <header class="category__header">
-            Бытовая техника
-        </header>
-        </div>`;
-    }
+    const categoriesMarkup = data
+      .map(dataItem => {
+        return `
+            <div class="category" data-id="${dataItem.id}">
+            <header class="category__header">
+                ${dataItem.title}
+            </header>
+            <div class="category__body">
+                <div class="subcategory-list">
+                    <ul class="sortable-list">
+                        ${this.getCategoriesListItem(dataItem)}
+                    </ul>
+                </div>
+            </div>
+            </div>`;
+      })
+      .join('');
 
-    getSubElements(element) {
-        const elements = element.querySelectorAll("[data-element]");
-        return [...elements].reduce((acc, item) => {
-          acc[item.dataset.element] = item;
-          return acc;
-        }, {});
-      }
+    categoriesContainer.innerHTML = categoriesMarkup;
+  }
 
+  getCategoriesListItem(data) {
+    const { subcategories } = data;
+
+    return subcategories
+      .map(subcategory => {
+        return `
+            <li class="categories__sortable-list-item sortable-list__item" data-grab-handle="" data-id="${subcategory.id}">
+                <strong>${subcategory.title}</strong>
+                <span><b>${subcategory.count}</b> products</span>
+            </li>`;
+      })
+      .join('');
+  }
+
+  getSubElements(element) {
+    const elements = element.querySelectorAll('[data-element]');
+    return [...elements].reduce((acc, item) => {
+      acc[item.dataset.element] = item;
+      return acc;
+    }, {});
+  }
+
+  initEventListeners() {
+
+  }
 }
